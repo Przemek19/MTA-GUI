63d4bb354f1097b415b10540ea01b75b09cde6a3
Pevo <38362954+Przemek19@users.noreply.github.com>
2019-06-21 11:15:45 +0200

    Add files via upload

---
+++ export_c.lua
@@ -0,0 +1,18 @@
+local prefix = gui.config.functionsPrefix or ""
+
+for i,v in pairs(gui.functions) do
+  if type(v) == "function" then
+    _G[i] = v --_G[prefix..i] = v
+    --outputChatBox('<export function="' .. prefix .. i .. '" type="client" />')
+    --outputChatBox('<export function="' .. i .. '" type="client" />')
+  end
+end
+
+for i,v in pairs(gui.globalFunctions) do
+  if type(v) == "function" then
+    _G[i] = v
+    --outputChatBox('<export function="' .. i .. '" type="client" />')
+  end
+end
+
+prefix = nil
